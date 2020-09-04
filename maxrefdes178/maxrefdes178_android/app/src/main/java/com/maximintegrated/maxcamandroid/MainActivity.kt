cab01b7fd1476bb39511a2697493cda40c32d82b
Meliksah.Cakir <Meliksah.Cakir@maximintegrated.com>
2020-09-04 20:53:22 +0300

    Kotlin project is created
    Change-Id: I23cfc7387deb432bb6a169edc8c67bb2840ab7f6

---
+++ maxrefdes178/maxrefdes178_android/app/src/main/java/com/maximintegrated/maxcamandroid/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.maximintegrated.maxcamandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
