ecc5a32ff240e438a6ed86493e95e1f0422e58db
Tugrul Yatagan <Tugrul.Yatagan@maximintegrated.com>
2020-09-16 16:18:11 +0300

    create .bin files from .elf

--- maxrefdes178/MAX78000/run.sh
+++ maxrefdes178/MAX78000/run.sh
@@ -1,1 +1,2 @@
-openocd -s C:/Maxim/Toolchain/share/openocd/scripts -f interface/cmsis-dap.cfg -f target/ai85.cfg -f target/ai85Base.cfg -c "program build/max78000.elf 0x0 verify reset exit"
+#openocd -s C:/Maxim/Toolchain/share/openocd/scripts -f interface/cmsis-dap.cfg -f target/ai85.cfg -f target/ai85Base.cfg -c "program build/max78000.elf 0x0 verify reset exit"
+openocd -s C:/Maxim/Toolchain/share/openocd/scripts -f interface/cmsis-dap.cfg -f target/ai85.cfg -f target/ai85Base.cfg -c "program build/max78000.bin verify reset exit 0x10000000"
