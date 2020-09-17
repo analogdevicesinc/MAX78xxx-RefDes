70b85f55707cf8cafd7c9ca0b544d862e382909f
Tugrul Yatagan <Tugrul.Yatagan@maximintegrated.com>
2020-09-17 15:38:15 +0300

    max32666 pmic file is added

--- maxrefdes178/maxcam_faceid_max32666/run.sh
+++ maxrefdes178/maxcam_faceid_max32666/run.sh
@@ -1,2 +1,3 @@
-#openocd -s C:/Maxim/Toolchain/share/openocd/scripts -f interface/cmsis-dap.cfg -f target/max32665.cfg -c "program build/maxcam_faceid_max32666.elf 0x0 verify reset exit"
-openocd -s C:/Maxim/Toolchain/share/openocd/scripts -f interface/cmsis-dap.cfg -f target/max32665.cfg -c "program build/maxcam_faceid_max32666.bin verify reset exit 0x10000000"
+openocd -s C:/Maxim/Toolchain/share/openocd/scripts -f interface/cmsis-dap.cfg -f target/maxcam.cfg -c  "halt;max32xxx mass_erase 0"
+#openocd -s C:/Maxim/Toolchain/share/openocd/scripts -f interface/cmsis-dap.cfg -f target/maxcam.cfg -c "program build/maxcam_faceid_max32666.elf 0x0 verify reset exit"
+openocd -s C:/Maxim/Toolchain/share/openocd/scripts -f interface/cmsis-dap.cfg -f target/maxcam.cfg -c  "program build/maxcam_faceid_max32666.bin verify reset exit 0x10000000"
