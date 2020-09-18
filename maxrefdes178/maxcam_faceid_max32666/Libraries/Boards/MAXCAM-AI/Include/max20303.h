916a0c686c04cae42a8f3395edb7fc2f7e605fae
Gokhan Bektas <gokhan.bektas@maximintegrated.com>
2020-09-18 16:39:32 +0300

    Added MAX20303 PMIC driver

---
+++ maxrefdes178/maxcam_faceid_max32666/Libraries/Boards/MAXCAM-AI/Include/max20303.h
@@ -0,0 +1,109 @@
+#ifndef MAX20303_DRIVER
+#define MAX20303_DRIVER
+
+#define MAX20303_ADDR 0x50
+#define HARDWARE_ID 0x02
+
+#define LDO_OUTPUT_ON			1
+#define LDO_OUTPUT_OFF			0
+
+#define BUCK_OUTPUT_ON			1
+#define BUCK_OUTPUT_OFF			0
+
+#define BOOST_OUTPUT_ON			1
+#define BOOST_OUTPUT_OFF		0
+
+#define LED_OUTPUT_ON			1
+#define LED_OUTPUT_OFF			0
+
+// Registers
+#define REG_HARDWARE_ID         0x00     // HardwareID Register
+#define REG_FIRMWARE_REV        0x01     // FirmwareID Register
+#define REG_INT0                0x03     // Int0 Register
+#define REG_INT1                0x04     // Int1 Register
+#define REG_INT2                0x05     // Int2 Register
+#define REG_STATUS0             0x06     // Status Register 0
+#define REG_STATUS1             0x07     // Status Register 1
+#define REG_STATUS2             0x08     // Status Register 2
+#define REG_STATUS3             0x09     // Status Register 2
+#define REG_SYSTEM_ERROR        0x0B     // SystemError Register
+#define REG_INT_MASK0           0x0C     // IntMask0 Register
+#define REG_INT_MASK1           0x0D     // IntMask1 Register
+#define REG_INT_MASK2           0x0E     // IntMask1 Register
+#define REG_AP_DATOUT0          0x0F     // APDataOut0 Register
+#define REG_AP_DATOUT1          0x10     // APDataOut1 Register
+#define REG_AP_DATOUT2          0x11     // APDataOut2 Register
+#define REG_AP_DATOUT3          0x12     // APDataOut3 Register
+#define REG_AP_DATOUT4          0x13     // APDataOut4 Register
+#define REG_AP_DATOUT5          0x14     // APDataOut5 Register
+#define REG_AP_DATOUT6          0x15     // APDataOut6 Register
+#define REG_AP_CMDOUT           0x17     // APCmdOut Register
+#define REG_AP_RESPONSE         0x18     // APResponse Register
+#define REG_AP_DATAIN0          0x19
+#define REG_AP_DATAIN1          0x1A
+#define REG_AP_DATAIN2          0x1B
+#define REG_AP_DATAIN3          0x1C
+#define REG_AP_DATAIN4          0x1D
+#define REG_AP_DATAIN5          0x1E
+#define REG_LDO_DIRECT          0x20
+#define REG_MPC_DIRECTWRITE     0x21
+#define REG_MPC_DIRECTRED       0x22
+#define REG_LED_STEP_DIRECT     0x2C
+#define REG_LED0_DIRECT         0x2D
+#define REG_LED1_DIRECT         0x2E
+#define REG_LED2_DIRECT         0x2F
+#define REG_LDO1_CONFIG_WRITE   0x40
+#define REG_LDO1_CONFIG_READ    0x41
+#define REG_LDO2_CONFIG_WRITE   0x42
+#define REG_LDO2_CONFIG_READ    0x43
+
+#define	APREG_GPIO_CONFIG_WRITE					0x01
+#define	APREG_GPIO_CONFIG_READ					0x02
+#define	APREG_GPIO_CONTROL_WRITE				0x03
+#define	APREG_GPIO_CONTROL_READ					0x04
+
+#define	APREG_MPC_CONFIG_WRITE					0x06
+#define	APREG_MPC_CONFIG_READ					0x07
+#define	APREG_INPUTCURRENT_CONFIG_WRITE			0x10
+#define	APREG_INPUTCURRENT_CONFIG_READ			0x11
+#define	APREG_THERMALSHUTDOWN_CONFIG_READ		0x12
+#define	APREG_CHARHER_CONFIG_WRITE				0x14
+#define	APREG_CHARHER_CONFIG_READ				0x15
+#define APREG_CHARGERTHERMALLIMITS_CONFIG_WRITE	0x16
+#define APREG_CHARGERTHERMALLIMITS_CONFIG_READ	0x17
+
+#define	APREG_BST_CONFIG_WRITE					0x30
+#define	APREG_BST_CONFIG_READ					0x31
+#define APREG_BUCK1_CONFIG_WRITE				0x35
+#define	APREG_BUCK1_CONFIG_READ					0x36
+#define APREG_BUCK1_DVSCONFIG_WRITE				0x37
+#define APREG_BUCK2_CONFIG_WRITE				0x3A
+#define APREG_BUCK2_CONFIG_READ					0x3B
+#define APREG_BUCK2_DVSCONFIG_WRITE				0x3C
+#define APREG_LDO1_CONFIG_WRITE					0x40
+#define APREG_LDO1_CONFIG_READ					0x41
+#define APREG_LDO2_CONFIG_WRITE					0x42
+#define APREG_LDO2_CONFIG_READ					0x43
+#define APREG_CHARGEPUMP_CONFIG_WRITE			0x46
+#define APREG_CHARGEPUMP_CONFIG_READ			0x47
+#define APREG_SFOUT_CONFIG_WRITE				0x48
+#define APREG_SFOUT_CONFIG_READ					0x49
+
+#define APREG_BBST_CONFIG_WRITE					0x70
+#define APREG_BBST_CONFIG_READ					0x71
+
+#define APREG_POWEROFF							0x80
+#define APREG_SOFTRESET							0x81
+#define APREG_HARDRESET							0x82
+#define APREG_STAYON							0x83
+#define APREG_POWEROFF_COMMAND_DELAY			0x84
+
+
+void MAX20303_initialize( uint8_t initialize_i2c );
+void MAX20303_ldo1( uint8_t ldo1_onoff );
+void MAX20303_worker();
+void MAX20303_setbuck1( uint8_t buck1_onoff);
+void MAX20303_setbuck2( uint8_t buck1_onoff);
+
+
+#endif /* MAX20303_DRIVER_H */
---
+++ maxrefdes178/maxcam_faceid_max32666/Libraries/Boards/MAXCAM-AI/Source/max20303.c
@@ -0,0 +1,212 @@
+/*******************************************************************************
+ * Copyright (C) £2015 Maxim Integrated Products, Inc., All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Except as contained in this notice, the name of Maxim Integrated
+ * Products, Inc. shall not be used except as stated in the Maxim Integrated
+ * Products, Inc. Branding Policy.
+ *
+ * The mere transfer of this software does not imply any licenses
+ * of trade secrets, proprietary technology, copyrights, patents,
+ * trademarks, maskwork rights, or any other form of intellectual
+ * property whatsoever. Maxim Integrated Products, Inc. retains all
+ * ownership rights.
+ *
+ * $Date: 2019-11-07 18:43:15 -0600 (Thu, 07 Nov 2019) $
+ * $Revision: 48537 $
+ *
+ ******************************************************************************/
+
+#include <stdio.h>
+#include "board.h"
+#include "mxc_config.h"
+#include "mxc_sys.h"
+#include "mxc_assert.h"
+#include "gpio.h"
+#include "mxc_pins.h"
+#include "spixfc.h"
+#include "tmr_utils.h"
+#include "max20303.h"
+#include "i2c.h"
+
+
+/**
+ * @brief   Writes a single PMIC register. Before calling this function, PMIC should
+ * be initialized via MAX20303_initialize() function
+ * @param      reg is the address of MAX20303 register
+ * @param      value is the register value to be written
+ * @returns    This function has no return value.
+ */
+static int MAX20303_writeReg(const uint8_t reg, const uint8_t value)
+{
+    const uint8_t cmdData[2] = {reg, value};
+    return I2C_MasterWrite(PMIC_I2C, MAX20303_ADDR, cmdData, 2, 0);
+}
+
+
+/**
+ * @brief   Initializes the I2C peripheral for PMIC communication if needed
+ * @param      initialize_i2c is determines whether the I2C peripheral to be initialized or not
+ * @returns    #E_NO_ERROR if everything is successful,
+ *             @ref MXC_Error_Codes if an error occurred
+ */
+void MAX20303_initialize( uint8_t initialize_i2c ) {
+
+	if ( initialize_i2c ) {
+	
+		I2C_Shutdown( PMIC_I2C );
+		I2C_Init( PMIC_I2C, I2C_FAST_MODE, NULL);
+		NVIC_EnableIRQ( PMIC_I2C_IRQ );
+
+	}
+
+
+
+	// Configure LDO and Buck outputs
+    TMR_Delay(MXC_TMR0, MSEC(10), 0);
+	MAX20303_setbuck1( BUCK_OUTPUT_ON );
+    TMR_Delay(MXC_TMR0, MSEC(10), 0);
+	MAX20303_setbuck2( BUCK_OUTPUT_ON );
+
+}
+
+/**
+ * @brief   Controls PMIC's RGB LED - Red Color. Before calling this function, PMIC should
+ * be initialized via MAX20303_initialize() function
+ * @param      ledStatus value 0 turns off the Red LED, non-zero value turns on the Red LED
+ * @returns    This function has no return value
+ */
+void MAX20303_led_red( uint8_t ledStatus ) {
+
+	if ( ledStatus == LED_OUTPUT_OFF )
+		MAX20303_writeReg( REG_LED1_DIRECT, 0x01 );
+	else
+		MAX20303_writeReg( REG_LED1_DIRECT, 0x21 );
+
+}
+
+/**
+ * @brief   Controls PMIC's RGB LED - Green Color. Before calling this function, PMIC should
+ * be initialized via MAX20303_initialize() function
+ * @param      ledStatus value 0 turns off the Green LED, non-zero value turns on the Red LED
+ * @returns    This function has no return value
+ */
+void MAX20303_led_green( uint8_t ledStatus ) {
+
+	if ( ledStatus == LED_OUTPUT_OFF )
+		MAX20303_writeReg( REG_LED2_DIRECT, 0x01 );
+	else
+		MAX20303_writeReg( REG_LED2_DIRECT, 0x21 );
+}
+
+/**
+ * @brief   Controls PMIC's RGB LED - Blue Color. Before calling this function, PMIC should
+ * be initialized via MAX20303_initialize() function
+ * @param      ledStatus value 0 turns off the Blue LED, non-zero value turns on the Red LED
+ * @returns    This function has no return value
+ */
+void MAX20303_led_blue( uint8_t ledStatus ) {
+
+	if ( ledStatus == LED_OUTPUT_OFF )
+		MAX20303_writeReg( REG_LED0_DIRECT, 0x01 );
+	else
+		MAX20303_writeReg( REG_LED0_DIRECT, 0x21 );
+}
+
+/**
+ * @brief  Setup for LDO1 of MAX20303 PMIC to enable or disable MAX78000 board 1.8V digital supply.
+ * LDO1 is configured as load-swittch. Before calling this function, PMIC should be initialized via
+ * MAX20303_initialize() function
+ * @param      Non-zero value configures LDO1 as load-switch but configures the state as "ON".
+ * Value 0 still configures LDO1 as load-switch and configures the state as "OFF.
+ /
+ * @returns    This function has no return value
+ */
+void MAX20303_ldo1( uint8_t ldo1_onoff )
+{
+
+	if ( ldo1_onoff == LDO_OUTPUT_OFF )
+		MAX20303_writeReg(REG_AP_DATOUT0, 0x04);  // LDO1Md:1 Load-switch mode.  LDO1En:01   Disabled
+	else
+		MAX20303_writeReg(REG_AP_DATOUT0, 0x05);  // LDO1Md:1 Load-switch mode.  LDO1En:01   Enabled
+
+
+    MAX20303_writeReg(REG_AP_DATOUT1, 0x34);      // LDO1Vset : 0x34 = 52.  ( 52 * 25mv) + 0.5V = 1.8V
+    MAX20303_writeReg(REG_AP_CMDOUT, APREG_LDO1_CONFIG_WRITE);
+
+}
+
+
+/**
+ * @brief  Setup for Buck1 of MAX20303 PMIC to enable or disable MAX78000 board 1.8V digital supply.
+ * Before calling this function, PMIC should be initialized via MAX20303_initialize() function
+ * @param      Non-zero value configures LDO1 as load-switch but configures the state as "ON".
+ * Value 0 still configures LDO1 as load-switch and configures the state as "OFF.
+ /
+ * @returns    This function has no return value
+ */
+void MAX20303_setbuck1( uint8_t buck1_onoff)
+{
+    MAX20303_writeReg(REG_AP_DATOUT0, 0x7E);  //
+    MAX20303_writeReg(REG_AP_DATOUT1, 0x28);  // Buck1Vset = 0x28=40    ( (40x25mV) + 0.8V = 1.8V)
+    MAX20303_writeReg(REG_AP_DATOUT2, 0x1F);  // Buck1Iset = 0b1111>375mA,  Buck1IZCSet=0b01>20mA   Buck1 ISet and Zero Crossing Threshold
+
+    if ( buck1_onoff == LDO_OUTPUT_OFF )
+    	MAX20303_writeReg(REG_AP_DATOUT3, 0x00);  // Buck1En = 0   Buck1 disabled
+    else
+    	MAX20303_writeReg(REG_AP_DATOUT3, 0x01);  // Buck1En = 1   Buck1 enabled
+
+    MAX20303_writeReg(REG_AP_CMDOUT, APREG_BUCK1_CONFIG_WRITE);
+}
+
+
+/**
+ * @brief  Setup for Buck2 of MAX20303 PMIC to enable or disable MAX78000 board 2.8V digital supply.
+ * Before calling this function, PMIC should be initialized via MAX20303_initialize() function
+ * @param      Non-zero value configures LDO1 as load-switch but configures the state as "ON".
+ * Value 0 still configures LDO1 as load-switch and configures the state as "OFF.
+ /
+ * @returns    This function has no return value
+ */
+void MAX20303_setbuck2( uint8_t buck1_onoff)
+{
+    MAX20303_writeReg(REG_AP_DATOUT0, 0x7E);  //
+    MAX20303_writeReg(REG_AP_DATOUT1, 0x28);  // Buck2Vset = 0x28=40    ( (40x50mV) + 0.8V = 2.8V)
+    MAX20303_writeReg(REG_AP_DATOUT2, 0x2F);  // Buck2Iset = 0b1111>375mA,  Buck2IZCSet=0b10>30mA   Buck2 ISet and Zero Crossing Threshold
+
+    if ( buck1_onoff == LDO_OUTPUT_OFF )
+    	MAX20303_writeReg(REG_AP_DATOUT3, 0x00);  //Buck2En = 0   Buck2 disabled
+    else
+    	MAX20303_writeReg(REG_AP_DATOUT3, 0x01);  //Buck2En = 1   Buck2 enabled
+
+    MAX20303_writeReg(REG_AP_CMDOUT, APREG_BUCK2_CONFIG_WRITE);
+}
+
+
+
+
+void MAX20303_worker()
+{
+	MAX20303_led_green( LED_OUTPUT_OFF );
+    TMR_Delay(MXC_TMR0, MSEC(500), 0);
+
+    MAX20303_led_green( LED_OUTPUT_ON );
+    TMR_Delay(MXC_TMR0, MSEC(500), 0);
+}
