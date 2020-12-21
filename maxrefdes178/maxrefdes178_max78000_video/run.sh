aa113e4d58e0c6deba0bafa4dfc136ec478f5208
Tugrul Yatagan <Tugrul.Yatagan@maximintegrated.com>
2020-12-21 17:40:30 +0300

    Update project configuration files for msdk

--- maxrefdes178/maxrefdes178_max78000_video/run.sh
+++ maxrefdes178/maxrefdes178_max78000_video/run.sh
@@ -1,2 +1,2 @@
-#openocd -s C:/Maxim/Toolchain/share/openocd/scripts -f interface/cmsis-dap.cfg -f target/ai85.cfg -f target/ai85Base.cfg -c "program build/maxrefdes178_max78000_video.elf 0x0 verify reset exit"
-openocd -s C:/Maxim/Toolchain/share/openocd/scripts -f interface/cmsis-dap.cfg -f target/ai85.cfg -f target/ai85Base.cfg -c "program build/maxrefdes178_max78000_video.bin verify reset exit 0x10000000"
+#openocd -s ${TOOLCHAIN_PATH}/OpenOCD/scripts -f interface/cmsis-dap.cfg -f target/max78000.cfg -c "program build/maxrefdes178_max78000_video.elf 0x0 verify reset exit"
+openocd -s ${TOOLCHAIN_PATH}/OpenOCD/scripts -f interface/cmsis-dap.cfg -f target/max78000.cfg -c "program build/maxrefdes178_max78000_video.bin verify reset exit 0x10000000"
