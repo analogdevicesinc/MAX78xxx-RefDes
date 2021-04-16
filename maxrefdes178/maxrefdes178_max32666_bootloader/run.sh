5e0fd92908dec4c487144bba85332374a4a6d067
Tugrul Yatagan <Tugrul.Yatagan@maximintegrated.com>
2021-04-16 15:39:22 +0300

    fix linter errors

--- maxrefdes178/maxrefdes178_max32666_bootloader/run.sh
+++ maxrefdes178/maxrefdes178_max32666_bootloader/run.sh
@@ -1,3 +1,5 @@
-openocd -s ${TOOLCHAIN_PATH}/OpenOCD/scripts -f interface/cmsis-dap.cfg -f target/max32665_nsrst.cfg -c "init;halt;max32xxx mass_erase 0;exit"
-#openocd -s ${TOOLCHAIN_PATH}/OpenOCD/scripts -f interface/cmsis-dap.cfg -f target/max32665_nsrst.cfg -c "program build/maxrefdes178_max32666_bootloader.elf 0x0 verify reset exit"
-openocd -s ${TOOLCHAIN_PATH}/OpenOCD/scripts -f interface/cmsis-dap.cfg -f target/max32665_nsrst.cfg -c "program build/maxrefdes178_max32666_bootloader.bin verify reset exit 0x10000000"
+#!/bin/sh
+
+openocd -s "${TOOLCHAIN_PATH}/OpenOCD/scripts" -f interface/cmsis-dap.cfg -f target/max32665_nsrst.cfg -c "init;halt;max32xxx mass_erase 0;exit"
+#openocd -s "${TOOLCHAIN_PATH}/OpenOCD/scripts" -f interface/cmsis-dap.cfg -f target/max32665_nsrst.cfg -c "program build/maxrefdes178_max32666_bootloader.elf 0x0 verify reset exit"
+openocd -s "${TOOLCHAIN_PATH}/OpenOCD/scripts" -f interface/cmsis-dap.cfg -f target/max32665_nsrst.cfg -c "program build/maxrefdes178_max32666_bootloader.bin verify reset exit 0x10000000"
